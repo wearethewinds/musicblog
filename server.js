e8287949e59088b2803a4726c50640cccf63f5e4
wearethewinds <fpotschies@gmail.com>
2013-12-25 15:34:52 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+	http = require('http');
+
+var app = express();
+app.configure(function() {
+	app.set('views', __dirname);
+	app.engine('html', require('ejs').renderFile);
+	app.use("/resources", express.static(__dirname + '/resources'));
+});
+
+app.get('/', function(req, res) {
+	res.render('index.html');
+});
+
+app.get('/review', function(req, res) {
+	res.render('review.html');
+});
+
+http.createServer(app).listen(8080);
