16a3fbc4d11a12378b8665544db41191062cb224
Fabian <fpotschies@gmail.com>
2014-04-14 22:16:15 +0200

    removed unnecessary parts

--- server_modules/latestreviews.js
+++ server_modules/latestreviews.js
@@ -7,30 +7,4 @@ exports.getLatestReviews = function(callback) {
             console.log(reviews);
            callback(reviews);
         });
-    /*callback([
-        {
-            artist: 'yat-kha',
-            recordname: 'tuva.rock',
-            dbref: 'yatkha-1999-tuvarock',
-            imgref: '/resources/images/covers/1.jpg'
-        },
-        {
-            artist: 'olan mill',
-            recordname: 'hiraeth',
-            dbref: 'olanmill-2013-hiraeth',
-            imgref: '/resources/images/covers/2.jpg'
-        },
-        {
-            artist: 'yndi halda',
-            recordname: 'enjoy eternal bliss',
-            dbref: 'yndihalda-2006-enjoyeternalbliss',
-            imgref: '/resources/images/covers/3.jpg'
-        },
-        {
-            artist: 'songs:ohia',
-            recordname: 'magnolia electric co.',
-            dbref: 'songsohia-2006-magnoliaelectricco',
-            imgref: '/resources/images/covers/4.jpg'
-        }
-    ]);*/
 };
