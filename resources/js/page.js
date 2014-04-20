41c4c2cb2907639f872472a9ca96cd7b5d4744d1
wearethewinds <fpotschies@gmail.com>
2014-04-20 16:47:46 +0200

    review aggregation on client side

--- resources/js/page.js
+++ resources/js/page.js
@@ -1,20 +1,32 @@
 (function($) {
-  var localStorageAvailable = (typeof LocalStorage !== 'undefined');
+  var localStorageAvailable = function () {
+      var test = 'test';
+      try {
+          localStorage.setItem(test, test);
+          localStorage.removeItem(test);
+          return true;
+      } catch(e) {
+          return false;
+      }
+  }();
   $(document).ready(function () {
        registerReview();
   });
 
   var registerReview = function () {
       var reviewsReadByUser,
-          dbReference = '';
+          url = window.location.href,
+          dbReference = url.split('/')[url.split('/').length - 1];
       // need to extract db reference
       if (localStorageAvailable) {
-          reviewsReadByUser = LocalStorage.reviews;
+          console.log(localStorage.getItem('reviews'));
+          reviewsReadByUser = JSON.parse(localStorage.getItem('reviews'));
           if (!reviewsReadByUser) {
               reviewsReadByUser = [];
           }
-          if (!reviewsReadByUser.contains(dbReference)) {
+          if ($.inArray(dbReference, reviewsReadByUser) < 0) {
               reviewsReadByUser.push(dbReference);
+              localStorage.setItem('reviews', JSON.stringify(reviewsReadByUser));
           }
       }
   };
