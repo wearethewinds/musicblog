e6a7a8e7d68fe0a3b6b6ffa1d0f88302b0612322
wearethewinds <fpotschies@gmail.com>
2016-04-05 19:59:25 +0200

    Add respond button to comment-directive

--- resources/js/app/directives/Comment.js
+++ resources/js/app/directives/Comment.js
@@ -1,8 +1,14 @@
 'use strict';
 
-module.exports = function () {
+module.exports = function ($compile) {
   return {
       replace: true,
-      template: '<article class="comment {{comment.slug}}"><header><time>{{comment.posted}}</time><h2>{{comment.author}}</h2></header><p>{{comment.text}}</p></article>'
+      template: '<article class="comment {{comment.slug}}"><header><time>{{comment.posted}}</time><h2>{{comment.author}}</h2></header><p>{{comment.text}}</p><p><a class="comment-response">Respond</a></p><new-comment dbrefer="{{comment.dbrefer}}" comments="comments" slug="{{comment.slug}}"></new-comment></article>',
+      link: function(scope, element) {
+          element.find('.comment-response').on('click', function () {
+              console.log('here');
+             element.find('.new-comment').css({'max-height': '500px', 'opacity': 1});
+          });
+      }
   }
 };
